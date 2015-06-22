2089d12299a393cc860754c4523ea04343e5a25c
Jérôme Prudent <jprudent@gmail.com>
2015-06-23 01:30:15 +0200

    finished + tests

--- src/consistent_hash/core.clj
+++ src/consistent_hash/core.clj
@@ -8,11 +8,15 @@
 
 (defn- hash-replica [hash-fn node ^long i]
   (-> node
-      (str (* 42 i))         ; add some entropy in case hash function is Object.hashCode
+      (str (if (zero? i) "" (* 42 i)))         ; 42 add some entropy in case hash function is Object.hashCode
       hash-fn))
 
 (defn- hash-replicas [hash-fn node ^long nb-replicas]
   (map #(hash-replica hash-fn node %) (range nb-replicas)))
+
+(defn find-closest-key [xs h]
+  (or (first (drop-while #(> h %) xs))
+      (first xs)))
 
 (defrecord CHR [nb-replicas hash-fn hmap]
   ConsistentHashRing
@@ -27,13 +31,7 @@
                #(apply dissoc % (hash-replicas hash-fn node nb-replicas))))
 
   (find-node [{:keys [hash-fn hmap]} data]
-    (loop [[x & rest :as xs] (keys hmap)
-           i (hash-fn data)]
-      (if (empty? xs)
-        nil
-        (if (>= i x)
-          x
-          (recur rest i))))))
+    (get hmap (find-closest-key (keys hmap) (hash-fn data)))))
 
 (def chr
   (->CHR 5 #(.hashCode %) (sorted-map)))
--- test/consistent_hash/core_test.clj
+++ test/consistent_hash/core_test.clj
@@ -5,28 +5,60 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]))
 
+(defn hashcode [o] (.hashCode o))
+
+(defn empty-chr [nb-replicas]
+  (->CHR nb-replicas hashcode (sorted-map)))
+
 (defspec adding-nodes-should-add-replicas
          100
          (prop/for-all [node (gen/not-empty gen/string)
                         nb-replicas gen/s-pos-int]
-                       (let [chr (->CHR nb-replicas #(.hashCode %) (sorted-map))
+                       (let [chr (empty-chr nb-replicas)
                              chr-added (add-node chr node)]
                          (= (count (:hmap chr-added)) nb-replicas))))
 
 (defspec adding-a-node-twice-has-no-effect-on-the-ring
          100
          (prop/for-all [node (gen/not-empty gen/string)
-                        nb-replicas gen/s-pos-int]
-                       (let [chr (->CHR nb-replicas #(.hashCode %) (sorted-map))
+                        nb-replicas gen/s-pos-int
+                        times gen/s-pos-int]
+                       (let [chr (empty-chr nb-replicas)
                              chr-added-once (add-node chr node)
-                             chr-added-twice (add-node chr-added-once node)]
-                         (= chr-added-once chr-added-twice))))
+                             chr-added-many-times (reduce #(add-node %1 %2) chr (repeat times node))]
+                         (= chr-added-once chr-added-many-times))))
 
 (defspec adding-then-removing-a-node-results-in-empty-ring
          100
          (prop/for-all [node (gen/not-empty gen/string)
                         nb-replicas gen/s-pos-int]
-                       (let [chr (->CHR nb-replicas #(.hashCode %) (sorted-map))
+                       (let [chr (empty-chr nb-replicas)
                              chr-added (add-node chr node)
                              chr-removed (remove-node chr-added node)]
                          (= chr chr-removed))))
+
+(defspec can-find-back-any-node-of-the-ring
+         100
+         (prop/for-all [node (gen/not-empty gen/string)
+                        nb-replicas gen/s-pos-int]
+                       (let [chr (-> (empty-chr nb-replicas)
+                                     (add-node node))]
+                         (= node (find-node chr node)))))
+
+(defspec there-is-always-a-node-for-a-given-data
+         100
+         (prop/for-all [nodes (gen/not-empty (gen/vector (gen/not-empty gen/string)))
+                        nb-replicas gen/s-pos-int
+                        data (gen/not-empty gen/string)]
+                       (let [chr (reduce #(add-node %1 %2) (empty-chr nb-replicas) nodes)]
+                         (not (nil? (find-node chr data))))))
+
+(defspec finding-node-in-ring-is-a-pure-function
+         100
+         (prop/for-all [node (gen/not-empty gen/string)
+                        nb-replicas gen/s-pos-int
+                        data (gen/not-empty gen/string)]
+                       (let [chr (-> (->CHR nb-replicas #(.hashCode %) (sorted-map))
+                                     (add-node node))]
+                         (= (find-node chr data) (find-node (-> (remove-node chr node)
+                                                                (add-node node)) data)))))

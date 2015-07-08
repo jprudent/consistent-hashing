5ce2165b4e0ae51fa6189b8ef44ded87deedd299
Jerome Prudent <jerome.prudent@vidal.fr>
2015-07-08 14:01:34 +0200

    simulator:

---
+++ src/consistent_hash/simulator.clj
@@ -0,0 +1,31 @@
+(ns consistent-hash.simulator
+  (:require [clojure.core.async :refer [chan pub sub >!! go go-loop <! >! timeout close!]]))
+
+(def request-channel (chan 100))
+(def response-channel (chan 100))
+(def pub-response-channel (pub response-channel :transaction-id))
+(def transaction-id (atom 0))
+
+(defrecord Request [url transaction-id])
+(defrecord Response [data transaction-id])
+
+(defn make-transation-id []
+  (swap! transaction-id inc))
+
+(defn subscribe [pub transaction-id]
+  (let [resp-chan (chan)]
+    (sub pub transaction-id resp-chan)
+    resp-chan))
+
+(defn browser-request [url]
+  (let [transaction-id (make-transation-id)
+        response-channel (subscribe pub-response-channel transaction-id)]
+    (>!! request-channel (->Request url transaction-id))
+    (go (println (<! response-channel))
+        (close! response-channel))))
+
+(defn server []
+  (go-loop [{:keys [transaction-id url]} (<! request-channel)]
+    (go (<! (timeout 1000))
+        (>! response-channel (->Response (str "data" transaction-id) transaction-id)))
+    (recur (<! request-channel))))
